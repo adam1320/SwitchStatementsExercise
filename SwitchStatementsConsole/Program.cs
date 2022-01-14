176151a3f9d7e95355ec7371c182ce7c0ed05584
Adam Brannon <adam1320@rocketmail.com>
2022-01-14 14:19:51 -0500

    initial commit

---
+++ SwitchStatementsConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchStatementsConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject");
+            string userFavoriteSubject = Console.ReadLine();
+
+            switch (userFavoriteSubject)
+            {
+                case "math":
+                    Console.WriteLine("Thats great, numbers are important!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("Thats great, proper grammar is important!");
+                    break;
+
+                case "history":
+                    Console.WriteLine("Thats great, knowledge of history is important!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Thats great, scientist are important!");
+                    break;
+
+                case "physical education":
+                    Console.WriteLine("Thats great, good health is important!");
+                    break;
+
+                default:
+                    Console.WriteLine("Thats great, all education is important!");
+                    break;
+
+            }
+        }
+    }
+}
